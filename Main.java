55d9d0b1a8cf8dad252a8e3c48db04f7c5dd141e
Suyash Singh <suyash.singh@cyfuture.com>
2024-10-21 14:27:19 +0530

    sum print

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main{
     public static void main(String args[]){
+int a=5;
+int b=10;
+int sum=a+b;
         System.out.print("Hello");
         System.out.println("How s you!!!!!");
+        System.out.println("sum"+sum);
     }
 }
